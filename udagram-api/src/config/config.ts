644cc52ad0d02aafcdb0ccca5d19955dab3a4741
NSimpsonsK <github@9542.de>
2022-03-30 22:04:16 +0200

    Revert "test"
    This reverts commit 1b628cc025a997563878ffdd0d9b28e5d205c245.

--- udagram-api/src/config/config.ts
+++ udagram-api/src/config/config.ts
@@ -5,11 +5,11 @@ export const config = {
   username: `${process.env.POSTGRES_USERNAME}`,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DB,
-  pg_port: Number(process.env.POSTGRES_PORT),
+  port: Number(process.env.PORT),
   host: process.env.POSTGRES_HOST,
   dialect: "postgres",
-  port: Number(process.env.PORT),
   aws_region: process.env.AWS_REGION,
+  aws_profile: process.env.AWS_PROFILE,
   aws_media_bucket: process.env.AWS_BUCKET,
   url: process.env.URL,
   jwt: {
--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,4 +1,13 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize(`postgres://${config.username}:${config.password}@${config.host}:${config.pg_port}/${config.database}`);
+export const sequelize = new Sequelize({
+  username: config.username,
+  password: config.password,
+  database: config.database,
+  host: config.host,
+  port: config.port,
+
+  dialect: "postgres",
+  storage: ":memory:",
+});
